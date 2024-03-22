3d1a1db01cebf1eae1306b69a65a6abca74d9319
Christian Nagel <christian@christiannagel.com>
2024-03-22 23:06:33 +0100

    remove app config and key vault

--- src/Codebreaker.AppHost/Program.cs
+++ src/Codebreaker.AppHost/Program.cs
@@ -8,12 +8,12 @@ builder.AddAzureProvisioning();
 //    .PublishAsAzureSqlDatabase()
 //    .AddDatabase("codebreaker");
 
-var appConfig = builder.AddAzureAppConfiguration("codebreakerconfig");
+//var appConfig = builder.AddAzureAppConfiguration("codebreakerconfig");
 
-var keyVault = builder.AddAzureKeyVault("codebreakervault");
+// var keyVault = builder.AddAzureKeyVault("codebreakervault");
 
-builder.AddProject<Projects.Codebreaker_InitalizeAppConfig>("initappconfig")
-    .WithReference(appConfig);
+//builder.AddProject<Projects.Codebreaker_InitalizeAppConfig>("initappconfig")
+//    .WithReference(appConfig);
 
 // the name needs to be reduced until this fix: https://github.com/Azure/azure-dev/issues/3496
 // don't use cb-cosmos, because of deploy failing with the '-' in the name
@@ -23,11 +23,11 @@ var cosmos = builder.AddAzureCosmosDB("cbcosmos")
 var gameAPIs = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
     // .WithReference(sqlServer)
     .WithReference(cosmos)
-    .WithReference(appConfig)
+ //   .WithReference(appConfig)
     .WithEnvironment("DataStore", dataStore);
 
 builder.AddProject<Projects.CodeBreaker_Bot>("bot")
-    .WithReference(appConfig)
+  //  .WithReference(appConfig)
     .WithReference(gameAPIs);
 
 // currently disabled - see https://github.com/PacktPublishing/Pragmatic-Microservices-with-CSharp-and-Azure/issues/81
